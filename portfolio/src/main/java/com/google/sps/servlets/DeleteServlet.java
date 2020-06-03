17b5323afc14ccf96829db5de13801098176f007
Sam Falberg <samuelfalberg@gmail.com>
2020-06-03 21:04:04 +0000

    Implement doPost to delete comments from server

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -19,11 +19,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
-/** Servlet that deletes all existing comments from the database*/
+/** Servlet that deletes a comment from the database*/
 @WebServlet("/delete-data")
 public class DeleteServlet extends HttpServlet {
+    
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        long id = Long.parseLong(request.getParameter("id"));
 
+        Key commentEntityKey = KeyFactory.createKey("Comment", id);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.delete(commentEntityKey);
     }
+}
