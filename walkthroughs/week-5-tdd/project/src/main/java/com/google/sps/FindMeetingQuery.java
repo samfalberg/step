6f755883a6692f0e50b020c71fec1fcf2744ae8d
Sam Falberg <samuelfalberg@gmail.com>
2020-06-19 17:32:31 +0000

    Change approach to focus on mandatory and optional events separately

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,9 +24,9 @@ import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    Collection<TimeRange> findMeeting = new ArrayList<TimeRange>();
-    
-    ArrayList<Event> eventsList = new ArrayList<Event>();
+   
+    ArrayList<Event> mandatoryEvents = new ArrayList<Event>();
+    ArrayList<Event> optionalEvents = new ArrayList<Event>();
     boolean isAttending = false;
     int totalEventTime = 0;
 
@@ -47,74 +47,81 @@ public final class FindMeetingQuery {
             }
         }
 
-        //Don't add event if it's too long to allow a meeting and only has optionals
-        if (event.getWhen().duration() + request.getDuration() > TimeRange.WHOLE_DAY.duration() && hasOnlyOptionals(event, request)) {
-            continue;
+        //Add to optional event list if not too long and attendees are only optionals
+        if (hasOnlyOptionals(event, request)) {
+            if (event.getWhen().duration() + request.getDuration() < TimeRange.WHOLE_DAY.duration()) {   
+                optionalEvents.add(event);
+            } else {
+                continue;
+            }
+        } else {   //Else add to mandatory event list
+            mandatoryEvents.add(event);
         }
 
-        eventsList.add(event);
         totalEventTime += event.getWhen().duration();
     }
 
     //Return empty list if meeting is longer than entire day
     if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
-        return findMeeting;
+        return new ArrayList<TimeRange>();
     }
 
     //If there are no events, or we're scheduling for people not attending, the whole day is free
     if (events.isEmpty() || !isAttending) {
-        findMeeting.add(TimeRange.WHOLE_DAY);
-        return findMeeting;
+        return Arrays.asList(TimeRange.WHOLE_DAY);
     } 
 
-    //The only events have optional attendees with no gaps in their schedules
-    if (eventsList.isEmpty()) {
-        return findMeeting;
+    //The only events have only optional attendees with no gaps in their schedules
+    if (mandatoryEvents.isEmpty() && optionalEvents.isEmpty()) {
+        return new ArrayList<TimeRange>();
     }
     
+    //Only optional events, find gaps in schedule
+    if (mandatoryEvents.isEmpty()) {
+        return freeTimes(optionalEvents, request);
+    } else {   //Includes mandatory events, find gaps in schedule and compare with optional gaps
+        return freeTimes(mandatoryEvents, request);
+    }
+    
+  }
+
+  /** 
+   * Finds valid time ranges for attendees, returns them as a collection
+   */
+  public Collection<TimeRange> freeTimes(ArrayList<Event> events, MeetingRequest request) {
+    Collection<TimeRange> freeTimes = new ArrayList<TimeRange>();
+
     //List of events ordered by start time
-    ArrayList<Event> startTimeEventsList = new ArrayList<Event>(eventsList);
-    Collections.sort(startTimeEventsList, Event.ORDER_BY_START);
+    ArrayList<Event> startTimeEvents = new ArrayList<Event>(events);
+    Collections.sort(startTimeEvents, Event.ORDER_BY_START);
 
     //List of events ordered by end time
-    ArrayList<Event> endTimeEventsList = new ArrayList<Event>(eventsList);
-    Collections.sort(endTimeEventsList, Event.ORDER_BY_END);
+    ArrayList<Event> endTimeEvents = new ArrayList<Event>(events);
+    Collections.sort(endTimeEvents, Event.ORDER_BY_END);
 
     //There's a valid event, so make time range from start til first event
-    findMeeting.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, startTimeEventsList.get(0).getWhen().start(), false));
+    freeTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, startTimeEvents.get(0).getWhen().start(), false));
 
     //If there's more than one event, and they don't overlap, get the time between them
-    if (startTimeEventsList.size() > 1) {
+    if (startTimeEvents.size() > 1) {
         
-        for (int i = 0; i < startTimeEventsList.size() - 1; i++) {
+        for (int i = 0; i < startTimeEvents.size() - 1; i++) {
             
-            if (!startTimeEventsList.get(i).getWhen().overlaps(startTimeEventsList.get(i+1).getWhen())) {
+            if (!startTimeEvents.get(i).getWhen().overlaps(startTimeEvents.get(i+1).getWhen())) {
                 
-                int currentEventEnd = startTimeEventsList.get(i).getWhen().end();
-                int nextEventStart = startTimeEventsList.get(i+1).getWhen().start();
+                int currentEventEnd = startTimeEvents.get(i).getWhen().end();
+                int nextEventStart = startTimeEvents.get(i+1).getWhen().start();
                 
-                findMeeting.add(TimeRange.fromStartEnd(currentEventEnd, nextEventStart, false));
-
-                //Optional-only event has made it so there isn't enough time for meeting
-                //Previous time range will get deleted in lines 117-120, add correct time range here
-                if (hasOnlyOptionals(startTimeEventsList.get(i), request) && 
-                    (totalEventTime + request.getDuration()) > TimeRange.WHOLE_DAY.duration()) {
-                    //If it's the first element of the list, time range must start at beginning of day
-                    int previousEventEnd = TimeRange.START_OF_DAY;
-                    if (i > 0) {
-                        previousEventEnd = startTimeEventsList.get(i - 1).getWhen().end();
-                    }
-                    findMeeting.add(TimeRange.fromStartEnd(previousEventEnd, nextEventStart, false));
-                }
+                freeTimes.add(TimeRange.fromStartEnd(currentEventEnd, nextEventStart, false));
             }
         }
     }
 
     //Make time range from end of last events til end of day
-    findMeeting.add(TimeRange.fromStartEnd(endTimeEventsList.get(eventsList.size() - 1).getWhen().end(), TimeRange.END_OF_DAY, true));
+    freeTimes.add(TimeRange.fromStartEnd(endTimeEvents.get(endTimeEvents.size() - 1).getWhen().end(), TimeRange.END_OF_DAY, true));
 
     //Remove all time ranges that are less than required meeting duration, return resulting list
-    return findMeeting
+    return freeTimes
             .stream()
             .filter(meeting -> meeting.duration() >= request.getDuration())
             .collect(Collectors.toList());
